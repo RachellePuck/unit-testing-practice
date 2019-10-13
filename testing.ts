83b9c9a588629f60bdf31e840ed221c70a1c7344
Puck van Zutphen <rpvzutphen@gmail.com>
2019-10-13 11:55:51 +0200

    Add tests for calculateAverage and reverse

--- testing.ts
+++ testing.ts
@@ -18,6 +18,12 @@ let tools = new CollectionTools();
 ​
 // Schrijf meer testen voor de functie calculate average
 assertTrue (tools.calculateAverage([1,2,3,4]) == 2.5);
+assertTrue (tools.calculateAverage([1,1,1,1]) == 1);
+assertTrue (tools.calculateAverage([1]) == 1);
+assertTrue (tools.calculateAverage([]) == null);
 ​
 // Schrijf meer testen voor de functie reverse
 assertTrue (tools.reverse([1,2,3,4]) == [4,3,2,1]);
+assertTrue (tools.reverse([11,22,33,44]) == [44,33,22,11]);
+assertTrue (tools.reverse([13]) == [13]);
+assertTrue (tools.reverse([]) == []);
